ea42d71e6adf33d86c82dd30b95e95f58a7b3340
Lucas <me@dotronglong.com>
2020-04-26 21:06:16 +0800

    v0.3.0

--- lib/src/firebase_facebook_authenticator.dart
+++ lib/src/firebase_facebook_authenticator.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:identity/identity.dart';
-import 'package:sso/sso.dart';
 
 class FirebaseFacebookAuthenticator
     with WillNotify, WillConvertUser
